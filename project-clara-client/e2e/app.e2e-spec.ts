19754b426ecc7bf80ff92137cd75bbcca3299b1c
TheDonDope <idontreallyneedthese@gmail.com>
2017-08-12 11:41:47 +0200

    chore(@angular/cli): update to 1.3.0
    Update to @angular/cli 1.3.0

--- project-clara-client/e2e/app.e2e-spec.ts
+++ project-clara-client/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ProjectClaraPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('project-clara App', () => {
-  let page: ProjectClaraPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ProjectClaraPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to cla!!');
+    expect(page.getParagraphText()).toEqual('Welcome to cla!');
   });
 });

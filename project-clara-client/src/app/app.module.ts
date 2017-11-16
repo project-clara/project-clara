e8a441f3d065cb2c348b66157c1f4552856a2377
TheDonDope <idontreallyneedthese@gmail.com>
2017-11-16 19:54:54 +0100

    fix(now-ui): fix now-ui setup
    - Remove unneccesary assets files
    - Use NPM packages instead
    - Update .angular-cli config with new styles and scripts
    - Update package-lock.json
    - Update yarn.lock

--- project-clara-client/src/app/app.module.ts
+++ project-clara-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -23,6 +24,8 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     // @angular/http
     HttpModule,
+    // @ng-bootstrap/ng-bootstrap
+    NgbModule.forRoot(),
     // app.routing
     AppRoutingModule,
     // core/core.module

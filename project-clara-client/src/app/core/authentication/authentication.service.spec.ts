224b7d597c7bc53435dc95864a1b584a095d27be
jonas <johnnyboy8591@gmail.com>
2017-11-21 00:24:23 +0100

    feat(ui, general): adapt backend behaviour for authentication service
    changing authentication.service and tests

--- project-clara-client/src/app/core/authentication/authentication.service.spec.ts
+++ project-clara-client/src/app/core/authentication/authentication.service.spec.ts
@@ -1,12 +1,17 @@
 /* tslint:disable:no-unused-variable */
 
-import { async, inject, TestBed } from '@angular/core/testing';
+import {async, inject, TestBed} from '@angular/core/testing';
+import {HttpClientModule, HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {environment} from '../../../environments/environment';
 
-import { AuthenticationService } from './authentication.service';
+
+import {AuthenticationService} from './authentication.service';
 
 describe('Service: Authentication', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
       providers: [AuthenticationService]
     });
   });
@@ -14,4 +19,26 @@ describe('Service: Authentication', () => {
   it('should ...', inject([AuthenticationService], (service: AuthenticationService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('it should login users, if x-auth-token will be returned',
+    async(
+      inject([AuthenticationService, HttpClient, HttpTestingController],
+        (authService: AuthenticationService, http: HttpClient, backend: HttpTestingController) => {
+          const username = 'test';
+          const password = 'password';
+          const token = 'ABCD-CDEF-0123-4567-89';
+          authService.login(username, password).subscribe(result => {
+              expect(authService.isUserLoggedIn()).toBe(true);
+              expect(authService.getCurrentUser().token).toBe(token);
+            }
+          );
+          const req = backend.expectOne(`${environment.apiBaseUrl}login`);
+          expect(req.request.method).toBe('POST');
+          expect(req.request.headers.get('Authorization')).toBe('Basic ' + btoa(username + ':' + password));
+          req.flush({}, {
+            headers: new HttpHeaders({'x-auth-token': token})
+          });
+        }
+      )
+    ));
 });

3de0c3470dd45c514971f7259bbf0c7f26e42fda
Edgar König <ede_99@gmx.de>
2017-11-24 15:48:44 +0100

    added jquery tooltip // removed Analytics // removed jquery carousel

--- project-clara-client/src/assets/now-ui/js/now-ui-kit.js
+++ project-clara-client/src/assets/now-ui/js/now-ui-kit.js
@@ -75,9 +75,9 @@ $(document).ready(function() {
     }
 
     // Activate Carousel
-    $('.carousel').carousel({
-        interval: 4000
-    });
+    // $('.carousel').carousel({
+    //     interval: 4000
+    // });
 
     $('.date-picker').each(function() {
         $(this).datepicker({
@@ -490,15 +490,15 @@ function debounce(func, wait, immediate) {
 };
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-46172202-1']);
-_gaq.push(['_trackPageview']);
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', 'UA-46172202-1']);
+// _gaq.push(['_trackPageview']);
 
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+//     var ga = document.createElement('script');
+//     ga.type = 'text/javascript';
+//     ga.async = true;
+//     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(ga, s);
+// })();
